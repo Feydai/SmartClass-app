a16f5db647e80d1b05b996624edfd53ec661890e
lsun <lsun.github@gmail.com>
2025-07-08 15:23:53 +0200

    add: setup project with navigation

---
+++ app/app/screens/screenAuth/screenAuth.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+// import { sceenAuthProps } from '../../types/screenProps';
+
+const ScreenAuth: React.FC = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  );
+};
+
+export default ScreenAuth;
---
+++ app/app/types/navigationProps.ts
@@ -0,0 +1,3 @@
+export type routeStackProps = {
+  RootStack: { ItemID: number };
+};
